aa17b6b4c4058a5b4aa7f44ebd465f58d3235bf9
Jovert Palonpon <palonponjovertlota@gmail.com>
2019-10-06 21:10:31 +0800

    wip

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output $OUTPUT_PATH $INPUT_PATH
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output $INPUT_PATH $OUTPUT_PATH
